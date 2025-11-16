b2e77b62d8555b430f5f1501d9c6947280ae0b90
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2025-11-16 16:49:11 +0000

    Add early version of feature flag generator
    Only top-level, primitive flag values are supported.

---
+++ FeatureFlags.Generation/JsonStructure/JsonStrucureParser.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.Json;
+
+namespace org.g14.FeatureFlags.Generation.JsonStructure;
+
+public static class JsonStrucureParser
+{
+    public static JsonType Parse(JsonElement element)
+    {
+        return element.ValueKind switch
+        {
+            JsonValueKind.Object => ParseObject(element),
+            JsonValueKind.Array => ParseArray(element),
+            _ => new JsonPrimitiveType(element.ValueKind)
+        };
+    }
+
+    private static JsonObjectType ParseObject(JsonElement element)
+    {
+        var obj = new JsonObjectType();
+
+        foreach (var prop in element.EnumerateObject())
+        {
+            obj.Properties[prop.Name] = Parse(prop.Value);
+        }
+
+        return obj;
+    }
+
+    private static JsonArrayType ParseArray(JsonElement element)
+    {
+        var arr = new JsonArrayType();
+
+        // If empty, treat as "array of any"
+        if (!element.EnumerateArray().Any())
+        {
+            arr.ItemType = new JsonPrimitiveType(JsonValueKind.Undefined);
+            return arr;
+        }
+
+
+        JsonType candidateType = Parse(element.EnumerateArray().First());
+        if (element.EnumerateArray().All(x => Parse(x) == candidateType))
+        {
+            arr.ItemType = candidateType;
+            return arr;
+        }
+        else
+        {
+            arr.ItemType = new JsonPrimitiveType(JsonValueKind.Undefined);
+            return arr;
+        }
+        // PERF: if checking every item is too expensive,
+        // we could refrain from looping over all items and just use candidateType immediately
+        // (i.e. assume other items are the same)
+    }
+}
--- UsageExample/Program.cs
+++ UsageExample/Program.cs
@@ -1,16 +1,18 @@
-using org.g14.FeatureFlags;
+// using org.g14.FeatureFlags;
+using org.g14.UsageExample;
 
 var builder = Host.CreateApplicationBuilder(args);
 
-builder.Services.AddSingleton<IFeatureManager, FeatureManager>();
+// builder.Services.AddSingleton<IFeatureManager, FeatureManager>();
 
 var host = builder.Build();
 
-IFeatureManager features = host.Services.GetService<IFeatureManager>()
-    ?? throw new InvalidOperationException("FeatureManager could not be retrieved from DI container");
+// IFeatureManager features = host.Services.GetService<IFeatureManager>()
+//     ?? throw new InvalidOperationException("FeatureManager could not be retrieved from DI container");
 
 Console.WriteLine("Hello, World!");
 
+/*
 if (features["TwoWayMessaging"].IsEnabled)
 {
     Console.WriteLine("Two way messaging");
@@ -27,5 +29,35 @@ if (features["IncomeSupport"].IsEnabled)
         Console.WriteLine("Page 8 of Income Support");
     }
 }
+*/
+
+// namespaces/classes for when we generate:
+// ...FeatureFlags.FlagsRoot
+// ...FeatureFlags.FlagsRoot.TwoWayMessagingFlags
+// ...FeatureFlags.FlagsRoot.IncomeSupportFlags
+// ...FeatureFlags.FlagsRoot.IncomeSupportFlags.Page8Flags
+
+// temporary: this will eventually be generated; located in .AddGeneratedFeatureFlags() service collection extension
+FlagsRoot features = host.Services.GetRequiredService<FlagsRoot>();
+
+features.TestBool = false;
+bool b = features.TestBool;
+features.TestBool = false; // TODO: assigning should not be possible (introducing the interfaces can fix that)
+/*
+if (features.IncomeSupport.IsEnabled)
+{
+    Console.WriteLine("Income Support");
+    int n = features.IncomeSupport.MagicNumber;
+    Console.WriteLine($"     -- magic number is {n}");
+
+    if (features.IncomeSupport.Page8.IsEnabled)
+    {
+        Console.WriteLine("Page 8 of Income Support");
+    }
+}
+*/
 
 // host.Run();
+
+// TODO: DOC: Limitation: all keys must be present in `appsettings.json`. Scanning other files might be added later.
+// TODO: generate the IFeatureManager interface; generate interfaces for all classes we want exposed to the dev
