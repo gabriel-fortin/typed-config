ac8b9ec34473f2ba351304798e11418786fd8f5b
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2025-11-16 21:31:10 +0000

    Extract core appsettings scanning and generation logic to separate file

---
+++ FeatureFlags.Generation/FeatureFlagTypesCreator.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections.Immutable;
+using System.Text.Json;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using org.g14.FeatureFlags.Generation.JsonStructure;
+
+namespace org.g14.FeatureFlags.Generation;
+
+public class FeatureFlagTypesCreator(
+    ImmutableArray<AdditionalText> appsettingsFiles,
+    string baseNamespace,
+    SourceProductionContext ctx)
+{
+    /// <summary>
+    /// Reads the structure of feature flags from appsettings and generates types representing them
+    /// </summary>
+    public void ScanAppsettingsAndCreateSourceFiles()
+    {
+        if (!IsFileCountValid(appsettingsFiles))
+        {
+            // TODO: generate a file with a COMPILATION_ERROR property having a value of "the implementation could not be generated"
+            return;
+        }
+
+        JsonType? parsedStructure = ReadAndParseAppsettings();
+        if (parsedStructure is not JsonObjectType jsonObject) return;
+
+        GenerateFeatureFlagClass(jsonObject, baseNamespace);
+        // TODO: generate array items and nested object items
+
+        GenerateUndefinedType();
+    }
+
+    private bool IsFileCountValid(ImmutableArray<AdditionalText> files)
+    {
+        if (files.Length == 0)
+        {
+            ctx.ReportDiagnostic(Diagnostic.Create(
+                descriptor: DiagnosticDescriptors.NotEnoughFiles,
+                location: null,
+                messageArgs: []));
+            return false;
+        }
+
+        if (files.Length != 1)
+        {
+            ctx.ReportDiagnostic(Diagnostic.Create(
+                descriptor: DiagnosticDescriptors.TooManyFiles,
+                location: null,
+                messageArgs: []));
+            return false;
+        }
+
+        return true;
+    }
+
+    private JsonType? ReadAndParseAppsettings()
+    {
+        SourceText? appsettingsSourceText = appsettingsFiles.First().GetText();
+        if (appsettingsSourceText == null)
+        {
+            // TODO: diagnostic: cannot read file
+            return null;
+        }
+
+        using JsonDocument appsettingsDoc = JsonDocument.Parse(appsettingsSourceText.ToString());
+        ctx.CancellationToken.ThrowIfCancellationRequested();
+        JsonElement featureFlagsSection = appsettingsDoc.RootElement.GetProperty("FeatureFlags");
+        JsonType jsonStructure = JsonStrucureParser.Parse(featureFlagsSection);
+        return jsonStructure;
+    }
+
+    private void GenerateFeatureFlagClass(JsonObjectType jsonStructure, string @namespace)
+    {
+        ctx.CancellationToken.ThrowIfCancellationRequested();
+
+        // TODO: add param for the name of the class
+
+        IEnumerable<KeyValuePair<string, JsonPrimitiveType>> props =
+            jsonStructure.Properties
+                .Where(x => x.Value is JsonPrimitiveType)
+                .Select(x => new KeyValuePair<string, JsonPrimitiveType>(x.Key, (JsonPrimitiveType)x.Value));
+
+        IEnumerable<string> propsLines = props
+            .Select(pair =>
+            {
+                string name = pair.Key;
+                (string type, string comment) = pair.Value.Kind switch
+                {
+                    JsonValueKind.String => ("string", string.Empty),
+                    JsonValueKind.Number => ("int", string.Empty),
+                    JsonValueKind.False or JsonValueKind.True => ("bool", string.Empty),
+                    _ => ("Undefined", $" // the unidentified JsonValueKind value was: {pair.Value.Kind}")
+                };
+                return $"public required {type} {name} {{ get; set; }}{comment}";
+            });
+
+
+        // TODO: order of members (once all are handled): primitives, arrays, nested objects
+        // TODO: PERF: use a string builder to build the class's code
+        ctx.AddSource(
+            hintName: "FlagsRoot.generated.cs",
+            source: $$"""
+                      namespace {{@namespace}};
+
+                      public class FlagsRoot
+                      {
+                          {{string.Join("\n    ", propsLines)}}
+                      }
+                      """);
+    }
+
+    private void GenerateUndefinedType()
+    {
+        ctx.CancellationToken.ThrowIfCancellationRequested();
+
+        ctx.AddSource(
+            hintName: "Undefined.generated.cs",
+            source: $$"""
+                      namespace {{baseNamespace}};
+
+                      /// <summary>
+                      /// The type of the item in appsettings could not be identified
+                      /// </summary>
+                      public class Undefined
+                      {
+                      }
+                      """);
+    }
+}
--- FeatureFlags.Generation/Generator.cs
+++ FeatureFlags.Generation/Generator.cs
@@ -1,8 +1,5 @@
 ﻿using System.Collections.Immutable;
-using System.Text.Json;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.Text;
-using org.g14.FeatureFlags.Generation.JsonStructure;
 
 namespace org.g14.FeatureFlags.Generation;
 
@@ -11,132 +8,38 @@ public class Generator : IIncrementalGenerator
 {
     public void Initialize(IncrementalGeneratorInitializationContext initContext)
     {
-        string baseNamespace = "org.g14.UsageExample";
-        // TODO: use the namespace of the consuming project + ".GeneratedFeatureFlags"
+        // prepare pipeline: getting the root namespace of the project using this generator
+        IncrementalValueProvider<string> rootNamespace =
+            initContext.AnalyzerConfigOptionsProvider
+                .Select(static (opts, _) =>
+                {
+                    if (opts.GlobalOptions.TryGetValue("build_property.RootNamespace", out var rn))
+                        return rn;
+                    if (opts.GlobalOptions.TryGetValue("build_property.AssemblyName", out var an))
+                        return an;
 
+                    return "Global";
+                });
+
+        // prepare pipeline: getting the appsettings files of the project using this generator
         IncrementalValueProvider<ImmutableArray<AdditionalText>> appsettingsFiles =
             initContext.AdditionalTextsProvider
                 .Where(static text => text.Path.EndsWith("appsettings.json"))
                 .Collect();
 
-        // TODO: make the lambda 'static', if possible
-        initContext.RegisterSourceOutput(appsettingsFiles, (ctx, files) =>
-        {
-            // TODO: extract the content of this lambda as a class; use ctx and files as properties
-
-            if (!IsFileCountValid(files, ctx))
+        // use the data from the pipelines to start generating source code
+        initContext.RegisterSourceOutput(
+            source: appsettingsFiles.Combine(rootNamespace),
+            action: (sourceProductionContext, input) =>
             {
-                // TODO: generate a file with a COMPILATION_ERROR property having a value of "the implementation could not be generated"
-                return;
-            }
+                ImmutableArray<AdditionalText> files = input.Left;
+                string @namespace = input.Right + ".GeneratedFeatureFlags";
 
-            JsonType? parsedStructure = ReadAndParseAppsettings(ctx, files);
-            if (parsedStructure is not JsonObjectType jsonObject) return;
-
-            GenerateFeatureFlagClass(ctx, jsonObject, baseNamespace);
-            // TODO: generare array items and nested object items
-
-            GenerateUndefinedType(ctx, baseNamespace);
-        });
-
-        // TODO: generate service collection extension method using RegisterPostInitializationOutput
-    }
-
-    private static void GenerateUndefinedType(SourceProductionContext ctx, string baseNamespace)
-    {
-        ctx.CancellationToken.ThrowIfCancellationRequested();
-
-        ctx.AddSource(
-            hintName: "Undefined.generated.cs",
-            source: $$"""
-                      namespace {{baseNamespace}};
-
-                      /// <summary>
-                      /// The type of the config item could not be identified
-                      /// </summary>
-                      public class Undefined
-                      {
-                      }
-                      """);
-    }
-
-    private static JsonType? ReadAndParseAppsettings(SourceProductionContext ctx, ImmutableArray<AdditionalText> txts)
-    {
-        SourceText? appsettingsSourceText = txts.First().GetText();
-        if (appsettingsSourceText == null)
-        {
-            // TODO: diagnostic: cannot read file
-            return null;
-        }
-
-        using JsonDocument appsettingsDoc = JsonDocument.Parse(appsettingsSourceText.ToString());
-        ctx.CancellationToken.ThrowIfCancellationRequested();
-        JsonElement featureFlagsSection = appsettingsDoc.RootElement.GetProperty("FeatureFlags");
-        JsonType jsonStructure = JsonStrucureParser.Parse(featureFlagsSection);
-        return jsonStructure;
-    }
-
-    private static void GenerateFeatureFlagClass(SourceProductionContext ctx, JsonObjectType jsonStructure,
-        string @namespace)
-    {
-        ctx.CancellationToken.ThrowIfCancellationRequested();
-
-        // TODO: add param for the name of the class
-
-        IEnumerable<KeyValuePair<string, JsonPrimitiveType>> props =
-            jsonStructure.Properties
-                .Where(x => x.Value is JsonPrimitiveType)
-                .Select(x => new KeyValuePair<string, JsonPrimitiveType>(x.Key, (JsonPrimitiveType)x.Value));
-
-        IEnumerable<string> propsLines = props
-            .Select(pair =>
-            {
-                string name = pair.Key;
-                (string type, string comment) = pair.Value.Kind switch
-                {
-                    JsonValueKind.String => ("string", string.Empty),
-                    JsonValueKind.Number => ("int", string.Empty),
-                    JsonValueKind.False or JsonValueKind.True => ("bool", string.Empty),
-                    _ => ("Undefined", $" // the unidentitied JsonValueKind value was: {pair.Value.Kind}")
-                };
-                return $$"""public required {{type}} {{name}} { get; set; }{{comment}}""";
+                // the core logic is hidden in here
+                var typesCreator = new FeatureFlagTypesCreator(files, @namespace, sourceProductionContext);
+                typesCreator.ScanAppsettingsAndCreateSourceFiles();
             });
 
-
-        // TODO: order of members (once all are handeled): primitives, arrays, nested objects
-        // TODO: PERF: use a string builder to build the class's code
-        ctx.AddSource(
-            hintName: "FlagsRoot.generated.cs",
-            source: $$"""
-                      namespace {{@namespace}};
-
-                      public class FlagsRoot
-                      {
-                          {{string.Join("\n    ", propsLines)}}
-                      }
-                      """);
-    }
-
-    private static bool IsFileCountValid(ImmutableArray<AdditionalText> txts, SourceProductionContext ctx)
-    {
-        if (txts.Length == 0)
-        {
-            ctx.ReportDiagnostic(Diagnostic.Create(
-                descriptor: DiagnosticDescriptors.NotEnoughFiles,
-                location: null,
-                messageArgs: []));
-            return false;
-        }
-
-        if (txts.Length != 1)
-        {
-            ctx.ReportDiagnostic(Diagnostic.Create(
-                descriptor: DiagnosticDescriptors.TooManyFiles,
-                location: null,
-                messageArgs: []));
-            return false;
-        }
-
-        return true;
+        // TODO: generate service collection extension method using RegisterPostInitializationOutput
     }
 }
