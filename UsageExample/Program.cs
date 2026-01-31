9bd1c31e2092e3975d61bfce0fb9a3fc01b0abf8
Gabriel Fortin <gabriel.14.fortin@gmail.com>
2026-01-31 22:11:28 +0000

    Move the core of the example into a separate class
    to better show real life use

--- UsageExample/Program.cs
+++ UsageExample/Program.cs
@@ -1,72 +1,18 @@
-// using org.g14.FeatureFlags;
-
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using UsageExample;
 using UsageExample.GeneratedTypedConfig;
-using UsageExample.GeneratedTypedConfig.Flags.TwoWayMessaging.RefreshIntervals;
 
-Console.WriteLine();
-
-/*
-// initialise host, DI container etc.
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddSingleton<IFeatureManager, FeatureManager>();
-var host = builder.Build();
-
-// retrieve dependency from DI container
-IFeatureManager features = host.Services.GetService<IFeatureManager>()
-    ?? throw new InvalidOperationException("FeatureManager could not be retrieved from DI container");
-
-// do work
-if (features["IncomeSupport"].IsEnabled)
-{
-    Console.WriteLine("Income Support");
-    int n = features["IncomeSupport"].Get<int>("MagicNumber");
-    Console.WriteLine($"    Magic number is {n}");
-
-    if (features["IncomeSupport.Page8"].IsEnabled)
-    {
-        Console.WriteLine("    Page");
-    }
-}
-
-if (features["TwoWayMessaging"].IsEnabled)
-{
-    Console.WriteLine("Two way messaging");
-}
-*/
-
-
-// initialise host, DI container etc.
+// initialise host, prepare DI container
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddTypedConfig();
-var host = builder.Build();
+builder.Services.TryAddTransient<ExampleClass>();
 
-// retrieve dependency from DI container
-TypedConfig config = host.Services.GetRequiredService<TypedConfig>();
+IHost host = builder.Build();
 
-// do work
-bool b = config.Flags.TestBool;
-if (config.Flags.IncomeSupport.IsEnabled)
-{
-    Console.WriteLine("Income Support");
-    int n = config.Flags.IncomeSupport.MagicNumber;
-    Console.WriteLine($"    Magic number is {n}");
+// execute the example
+ExampleClass clazz = host.Services.GetRequiredService<ExampleClass>();
+clazz.ExampleMethod();
 
-    if (config.Flags.IncomeSupport.Page8.IsEnabled)
-    {
-        Console.WriteLine("    Page 8");
-    }
-}
 
-if (config.Flags.TwoWayMessaging.IsEnabled)
-{
-    Console.WriteLine("Two Way Messaging");
-    Console.WriteLine("    Refresh intervals:");
-    foreach (RefreshIntervalsItemType interval in config.Flags.TwoWayMessaging.RefreshIntervals)
-    {
-        Console.Out.WriteLine($"        {interval.Interval}s, {interval.Repeats} times");
-    }
-}
-
-// host.Run();
 
 
